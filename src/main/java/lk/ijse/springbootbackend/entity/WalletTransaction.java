f8773aaec605ab1a816e68476c18bc77bdd765bb
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:25:44 +0530

    feat: add WalletTransaction entity with fields for transaction management

--- src/main/java/lk/ijse/springbootbackend/entity/WalletTransaction.java
+++ src/main/java/lk/ijse/springbootbackend/entity/WalletTransaction.java
@@ -1,4 +1,23 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class WalletTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+    private Long walletId;
+    private Long bookingId;
+    private String transactionDate;
+    private String transactionTime;
+    private String referenceNumber;
+    private String status;
 }
