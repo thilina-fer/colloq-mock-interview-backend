addac6e2a5d9e706ffe83b97323c6bd813b38131
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:19:42 +0530

    feat: add Interviewer entity with fields for interviewer management

--- src/main/java/lk/ijse/springbootbackend/entity/Interviewer.java
+++ src/main/java/lk/ijse/springbootbackend/entity/Interviewer.java
@@ -1,4 +1,28 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Interviewer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long interviewerId;
+    private Long authId;
+    private String bio;
+    private String company;
+    private String designation;
+    private Integer experienceYears;
+    private String specialization;
+    private String githubUrl;
+    private String linkedinUrl;
+    private String profilePicture;
+    private String status;
 }
