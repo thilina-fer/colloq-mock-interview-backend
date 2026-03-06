ddef1000ddbbcb31ccfd3080002b7490a634021b
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:25:30 +0530

    feat: add Wallet entity with fields for wallet management

--- src/main/java/lk/ijse/springbootbackend/entity/Wallet.java
+++ src/main/java/lk/ijse/springbootbackend/entity/Wallet.java
@@ -1,4 +1,22 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long walletId;
+    private Long interviewerId;
+    private Long bankAccountId;
+    private Double amount;
+    private String status;
 }
