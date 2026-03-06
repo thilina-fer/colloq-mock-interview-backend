5fa509e4ade45f6fefc9f4d52e4ddf3de1cbd0cc
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:20:01 +0530

    feat: add Level entity with fields for level management

--- src/main/java/lk/ijse/springbootbackend/entity/Level.java
+++ src/main/java/lk/ijse/springbootbackend/entity/Level.java
@@ -1,4 +1,22 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Level {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long levelId;
+    private String description;
+    private String sessionDuration;
+    private Double price;
+    private String status;
 }
