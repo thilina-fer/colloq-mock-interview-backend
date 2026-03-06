2c190035cac82eb0b8aba2f6c217bc445fa0d833
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:25:12 +0530

    feat: add SessionFeedback entity with fields for feedback management

--- src/main/java/lk/ijse/springbootbackend/entity/SessionFeedback.java
+++ src/main/java/lk/ijse/springbootbackend/entity/SessionFeedback.java
@@ -1,4 +1,22 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class SessionFeedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedbackId;
+    private Long bookingId;
+    private Integer rating;
+    private String feedbackComment;
+    private String status;
 }
