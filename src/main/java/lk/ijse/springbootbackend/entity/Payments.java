62f569cd17a5a5de80c58f60da496e2ee7c70bc2
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:21:11 +0530

    feat: add Payments entity with fields for payment management

--- src/main/java/lk/ijse/springbootbackend/entity/Payments.java
+++ src/main/java/lk/ijse/springbootbackend/entity/Payments.java
@@ -1,4 +1,20 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+    private Long bookingId;
+    private String paymentType;
+    private String status;
 }
