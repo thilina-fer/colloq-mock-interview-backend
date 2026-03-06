5e5631bce13be40d633115ca9f4617e353756891
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:46:25 +0530

    feat: add Auth relationship to Candidate entity

--- src/main/java/lk/ijse/springbootbackend/entity/Candidate.java
+++ src/main/java/lk/ijse/springbootbackend/entity/Candidate.java
@@ -1,9 +1,6 @@
 package lk.ijse.springbootbackend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -22,4 +19,8 @@ public class Candidate {
     private String linkedinUrl;
     private String profilePicture;
     private String status;
+
+    @OneToOne
+    @JoinColumn(name = "authId")
+    private Auth auth;
 }
