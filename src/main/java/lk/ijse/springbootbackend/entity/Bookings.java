fd8cbb2a459e31ede2f4bc697144bf7dcd3cda65
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:12:26 +0530

    feat: add Bookings entity with fields for booking management

--- src/main/java/lk/ijse/springbootbackend/entity/Bookings.java
+++ src/main/java/lk/ijse/springbootbackend/entity/Bookings.java
@@ -1,4 +1,27 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bookings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+    private Long candidateId;
+    private Long interviewerId;
+    private Long levelId;
+    private String jobType;
+    private String scheduleDate;
+    private String scheduleTime;
+    private String candidateNote;
+    private String meetingLink;
+    private String status;
 }
