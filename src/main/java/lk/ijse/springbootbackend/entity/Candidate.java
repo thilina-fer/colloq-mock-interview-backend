ead12f9d97732023340b63409398554fea9a5672
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:17:12 +0530

    feat: add Candidate entity with fields for candidate management

--- src/main/java/lk/ijse/springbootbackend/entity/Candidate.java
+++ src/main/java/lk/ijse/springbootbackend/entity/Candidate.java
@@ -1,4 +1,25 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long candidateId;
+    private Long authId;
+    private String joinDate;
+    private String bio;
+    private String githubUrl;
+    private String linkedinUrl;
+    private String profilePicture;
+    private String status;
 }
