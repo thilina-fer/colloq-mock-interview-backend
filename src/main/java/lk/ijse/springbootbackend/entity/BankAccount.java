4547b3a4e1f6bb23c6c562798edc62f2612d0f41
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-06 22:11:04 +0530

    feat: add BankAccount entity with fields for account management

--- src/main/java/lk/ijse/springbootbackend/entity/BankAccount.java
+++ src/main/java/lk/ijse/springbootbackend/entity/BankAccount.java
@@ -1,4 +1,23 @@
 package lk.ijse.springbootbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class BankAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bankAccountId;
+    private Long interviewerId;
+    private String bankName;
+    private String accountNumber;
+    private Boolean isDefault;
+    private String status;
 }
