dee2d44a61f2481cb2160fa12afed79aff0e627d
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-07 07:43:24 +0530

    refactor: rename ApiResponse class to APIResponse for consistency

--- src/main/java/lk/ijse/springbootbackend/dto/APIResponse.java
+++ src/main/java/lk/ijse/springbootbackend/dto/APIResponse.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @AllArgsConstructor
 @Data
-public class ApiResponse {
+public class APIResponse {
     private int statusCode;
     private String message;
     private Object data;
