a7b2c0d8bb226cb893635fadc4dcc1de2d6012a3
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-11 21:39:15 +0530

    feat: add authenticateWithGoogle method to AuthService for Google authentication

--- src/main/java/lk/ijse/springbootbackend/service/AuthService.java
+++ src/main/java/lk/ijse/springbootbackend/service/AuthService.java
@@ -2,9 +2,11 @@ package lk.ijse.springbootbackend.service;
 
 import lk.ijse.springbootbackend.dto.auth.AuthDTO;
 import lk.ijse.springbootbackend.dto.auth.AuthResponseDTO;
+import lk.ijse.springbootbackend.dto.auth.GoogleAuthDTO;
 import lk.ijse.springbootbackend.dto.auth.RegisterDTO;
 
 public interface AuthService {
     AuthResponseDTO authenticate(AuthDTO authDTO);
     String register(RegisterDTO registerDTO);
+    AuthResponseDTO authenticateWithGoogle(GoogleAuthDTO googleAuthDTO);
 }
