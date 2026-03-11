4b831a1501b8e9ecae99bad0af6f5c5f875e265e
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-11 18:50:17 +0530

    feat: add joinSDate field and Lombok @Data annotation to Interviewer entity

--- src/main/java/lk/ijse/springbootbackend/entity/Interviewer.java
+++ src/main/java/lk/ijse/springbootbackend/entity/Interviewer.java
@@ -2,17 +2,20 @@ package lk.ijse.springbootbackend.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Interviewer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long interviewerId;
 //    private Long authId;
+    private String joinSDate;
     private String bio;
     private String company;
     private String designation;
