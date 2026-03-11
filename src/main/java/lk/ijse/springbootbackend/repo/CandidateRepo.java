cea18029c8ba48aecae7beafd5e99618b4512af5
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-11 16:27:59 +0530

    refactor: refactor CandidateRepo to extend JpaRepository for enhanced functionality

--- src/main/java/lk/ijse/springbootbackend/repo/CandidateRepo.java
+++ src/main/java/lk/ijse/springbootbackend/repo/CandidateRepo.java
@@ -2,11 +2,11 @@ package lk.ijse.springbootbackend.repo;
 
 import lk.ijse.springbootbackend.entity.Auth;
 import lk.ijse.springbootbackend.entity.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface CandidateRepo extends CrudRepository<Candidate, Long> {
-    Optional<Candidate> findByAuth(Auth auth);
+public interface CandidateRepo extends JpaRepository<Candidate, Long> {
     boolean existsByAuth(Auth auth);
 }
