a666865aa140e84687825bdc751cf7d0b25723ce
thilina-fer <dilshanthilina1010@gmail.com>
2026-03-31 09:12:48 +0530

    feat: add getBookingsForCandidate method in BookingService for retrieving bookings by candidate username

--- src/main/java/lk/ijse/springbootbackend/service/BookingService.java
+++ src/main/java/lk/ijse/springbootbackend/service/BookingService.java
@@ -12,4 +12,5 @@ public interface BookingService {
     public List<BookingDTO> getBookingsByInterviewer(String username);
     List<BookingDTO> getBookingsByCandidate(Long candidateId);
     List<BookingDTO> getApprovedBookingsForInterviewer(String username);
+    List<BookingDTO> getBookingsForCandidate(String username);
 }
