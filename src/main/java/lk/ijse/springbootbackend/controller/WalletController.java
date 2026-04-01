7a6a9a3f5989416cc7a0469451475faffcd44591
thilina-fer <dilshanthilina1010@gmail.com>
2026-04-01 22:25:47 +0530

    feat: add getHistory endpoint in WalletController for retrieving withdrawal history

--- src/main/java/lk/ijse/springbootbackend/controller/WalletController.java
+++ src/main/java/lk/ijse/springbootbackend/controller/WalletController.java
@@ -2,11 +2,14 @@ package lk.ijse.springbootbackend.controller;
 
 import lk.ijse.springbootbackend.dto.APIResponse;
 import lk.ijse.springbootbackend.dto.WalletDTO;
+import lk.ijse.springbootbackend.dto.WithdrawalHistoryDTO;
 import lk.ijse.springbootbackend.service.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/wallet")
@@ -29,5 +32,11 @@ public class WalletController {
     ) {
         String message = walletService.withdrawFunds(amount, auth.getName());
         return ResponseEntity.ok(new APIResponse(200, message, null));
+    }
+
+    @GetMapping("/withdrawal-history")
+    public ResponseEntity<APIResponse> getHistory(Authentication auth) {
+        List<WithdrawalHistoryDTO> history = walletService.getWithdrawalHistory(auth.getName());
+        return ResponseEntity.ok(new APIResponse(200, "Success", history));
     }
 }
