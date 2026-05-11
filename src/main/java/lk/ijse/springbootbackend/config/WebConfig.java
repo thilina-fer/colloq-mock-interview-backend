1c72ea9edeafddcf454f7a43da163cab13442c00
thilina-fer <dilshanthilina1010@gmail.com>
2026-05-11 08:46:17 +0530

    feat: update CORS configuration to allow requests from React frontend

--- src/main/java/lk/ijse/springbootbackend/config/WebConfig.java
+++ src/main/java/lk/ijse/springbootbackend/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // හැම endpoint එකකටම
-                .allowedOrigins("http://localhost:5173") // ඔයාගේ React URL එක
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
